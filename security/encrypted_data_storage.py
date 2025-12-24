936ec6b1d79aa1ea2f771c060750217da2ecca16
kamiroubioum80 <98597234+kamiroubioum80@users.noreply.github.com>
2025-12-24 14:22:11 +0100

    Update encrypted_data_storage.pyhttps://organicagropastoralpi.com

--- security/encrypted_data_storage.py
+++ security/encrypted_data_storage.py
@@ -1,4 +1,4 @@
-import cryptography
+ https://organicagropastoralpi.com    import cryptography
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
