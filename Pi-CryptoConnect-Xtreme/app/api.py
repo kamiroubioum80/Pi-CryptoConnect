d0064e7bdda8079f16605c899d0455630fa12b3b
kamiroubioum80 <98597234+kamiroubioum80@users.noreply.github.com>
2025-12-24 13:33:45 +0100

    Update api.pyhttps://organicagropastoralpi.com

--- Pi-CryptoConnect-Xtreme/app/api.py
+++ Pi-CryptoConnect-Xtreme/app/api.py
@@ -1,4 +1,4 @@
-import requests
+https://organicagropastoralpi.com   import requests
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from app.exchange import Exchange
